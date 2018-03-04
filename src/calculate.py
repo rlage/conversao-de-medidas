af1528875629e9e9f354114a2bc4cd17c7e64900
Rafael Lage Tavares <rlt.rafael@gmail.com>
2018-03-04 20:24:04 -0300

    Return correct format str

--- src/calculate.py
+++ src/calculate.py
@@ -3,7 +3,7 @@ def calculateMeasure(obj):
   if type(values) is dict:
     for key in values.keys():
       if (values[key]['value'] >= 1):
-        return (values[key]['value'], values[key]['unit'])
-    return (values[0]['value'], values[0]['unit'])
+        return str(values[key]['value']) + " " + values[key]['unit']
+    return str(values[0]['value']) + " " + values[0]['unit']
   else:
-    return values
+    return obj
